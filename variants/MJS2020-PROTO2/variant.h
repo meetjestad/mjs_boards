260c9707be96975c0765c86e2b641758aeaa1531
Matthijs Kooijman <matthijs@stdin.nl>
2020-12-16 03:19:28 +0100

    Complete MJS2020 pin mapping
    This:
     - Adds all missing pins
     - Reorders all pins, removing unused pin numbers
     - Enables all ADC channels
     - Enables PWM where possible
     - Configures Serial2 (USART4)
     - Enables the DAC pins
     - Define constants as an enum instead of macros where appropriate.

--- variants/MJS2020-PROTO2/variant.h
+++ variants/MJS2020-PROTO2/variant.h
@@ -68,149 +68,185 @@ extern "C"
  *        Pins
  *----------------------------------------------------------------------------*/
 
+// Map chip pin names to the corresponding Arduino logical pin number
 enum {
-    PB6 = 0,
-    PB7 = 1,
+    PB6  = 0,
+    PB7  = 1,
     PB13 = 2,
     PB12 = 3,
     PB10 = 4,
     PB11 = 5,
     PB15 = 6,
     PB14 = 7,
-    PA0 = 8,
-    PB8 = 9,
-    PA15 = 10,
-    PB5 = 11,
-    PB4 = 12,
-    PB3 = 13,
-    // These are mapped twice
-    PC4 = 16,
-    PC5 = 17,
-    /* 18 - 21 are unused? */
-    PA11 = 22,
-    PA12 = 23,
-    PA7 = 24,
-    PB1 = 25,
-    PB2 = 26,
-    PA8 = 27,
-    PC10 = 28,
-    PC11 = 29,
-    PA2 = 30,
-    PA3 = 31,
-    PD2 = 32,
-    PC7 = 33,
-    PC8 = 34,
-    PC6 = 35,
-    PA9 = 36,
+    PB8  = 8,
+    PB9  = 9,
+    PC7  = 10,
+    PC8  = 11,
+    PC6  = 12,
+    PC10 = 13,
+    PC11 = 14,
+    PA8  = 15,
+    PB0  = 16,
+    PA5  = 17,
+    PA4  = 18,
+    PA1  = 19,
+    PA0  = 20,
+    PC0  = 21,
+    PC1  = 22,
+    PC4  = 23,
+    PC5  = 24,
+    PC9  = 25,
+    PA6  = 26,
+    PC3  = 27,
+    PD2  = 28,
+    PA15 = 29,
+    PA7  = 30,
+    PB1  = 31,
+    PB2  = 32,
+    PB5  = 33,
+    PB4  = 34,
+    PB3  = 35,
+    PA9  = 36,
     PA10 = 37,
-    PC9 = 38,
+    PA2  = 38,
+    PA3  = 39,
+    PC2  = 40,
+    PA13 = 41,
+    PA14 = 42,
+    PINS_COUNT,
+    // Not available as GPIO pins
+    // PA11 = 43,
+    // PA12 = 44,
+    // PC14 = 45,
+    // PC15 = 46,
+    // PC12 = 47,
+    // PC13 = 48,
+    // PH0  = 49,
+    // PH1  = 50,
 };
 
-// Number of pins defined in PinDescription array
-#define PINS_COUNT           (39u)
-#define NUM_DIGITAL_PINS     (31u)
-#define NUM_ANALOG_INPUTS    (6u)
-#define NUM_ANALOG_OUTPUTS   (0u)
+// Define friendly names for all pins
+enum {
+    // Digital pins
+    PIN_D0                = PB6,
+    PIN_WIRE_SCL          = PB6,
+    PIN_D1                = PB7,
+    PIN_WIRE_SDA          = PB7,
+    PIN_D2                = PB13,
+    PIN_SPI1_SCK          = PB13,
+    PIN_D3                = PB12,
+    PIN_D4                = PB10,
+    PIN_WIRE1_SCL         = PB10,
+    PIN_D5                = PB11,
+    PIN_WIRE1_SDA         = PB11,
 
-// LEDs
-#define PIN_LED_RED          (PC7)
-#define PIN_LED_GREEN        (PC8)
-#define PIN_LED_BLUE         (PC6)
-#define LED_BUILTIN          PIN_LED_RED
+    PIN_D6                = PB15,
+    PIN_SPI1_MOSI         = PB15,
+    PIN_D7                = PB14,
+    PIN_SPI1_MISO         = PB14,
+    PIN_D8                = PB8,
+    PIN_D9                = PB9,
 
-// Regulator enable pins
-#define PIN_ENABLE_5V        (PC10)
-#define PIN_ENABLE_3V_SENS   (PC11)
-#define PIN_ENABLE_3V_GPS    (PA8)
+    // LEDs
+    PIN_LED_RED           = PC7,
+    PIN_LED_GREEN         = PC8,
+    PIN_LED_BLUE          = PC6,
 
-// LoRa Radio pins
-#define PIN_LORA_SS          (PA15)
-#define PIN_LORA_RST         (PA7)
-#define PIN_LORA_DIO1        (PB1)
-#define PIN_LORA_BUSY        (PB2)
+    // Regulator enable pins
+    PIN_ENABLE_5V         = PC10,
+    PIN_ENABLE_3V_SENS    = PC11,
+    PIN_ENABLE_3V_GPS     = PA8,
 
-#define PIN_BATTERY          (PC4)
-#define PIN_SOLAR            (PC5)
-#define PIN_BOARD_ID         (PB0)
+    // Analog pins
+    // There are more analog inputs, these are just the ones labeled Ax
+    PIN_A0                = PB0,
+    PIN_BOARD_ID          = PB0,
+    PIN_A1                = PA5,
+    PIN_DAC1              = PA5,
+    PIN_A2                = PA4,
+    PIN_DAC0              = PA4,
+    PIN_A3                = PA1,
+    PIN_SERIAL3_RX        = PA1,
+    PIN_A4                = PA0,
+    PIN_SERIAL3_TX        = PA0,
+    PIN_A5                = PC0,
+    PIN_WIRE2_SCL         = PC0,
+    PIN_A6                = PC1,
+    PIN_WIRE2_SDA         = PC1,
 
-/*
- * Analog pins
- */
-#define PIN_A0               (16ul)
-#define PIN_A1               (17ul)
-#define PIN_A2               (18ul)
-#define PIN_A3               (19ul)
-#define PIN_A4               (20ul)
-#define PIN_A5               (21ul)
 
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
-static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
-static const uint8_t A5  = PIN_A5;
+    // Battery pins
+    PIN_BATTERY           = PC4,
+    PIN_SOLAR             = PC5,
+    PIN_CHARGE_STATUS     = PC9,
+
+    // Lux measurement
+    PIN_LUX_RANGE         = PA6,
+    PIN_LUX_ADC           = PC3,
+
+    // Button
+    PIN_BUTTON            = PD2,
+
+    // LoRa Radio pins
+    PIN_LORA_SS           = PA15,
+    PIN_LORA_RST          = PA7,
+    PIN_LORA_DIO1         = PB1,
+    PIN_LORA_BUSY         = PB2,
+
+    // SPI
+    PIN_SPI_MISO          = PB4,
+    PIN_SPI_MOSI          = PB5,
+    PIN_SPI_SCK           = PB3,
+
+    PIN_SERIAL1_RX        = PA10,
+    PIN_SERIAL1_TX        = PA9,
+
+    PIN_SERIAL2_RX        = PA3,
+    PIN_SERIAL2_TX        = PA2,
+
+    PIN_SWDIO             = PA13,
+    PIN_SWDCLK            = PA14,
+};
+
+// Define even shorter names for some pins (these are names commonly
+// defined by other Arduino cores as well).
+enum {
+    A0  = PIN_A0,
+    A1  = PIN_A1,
+    A2  = PIN_A2,
+    A3  = PIN_A3,
+    A4  = PIN_A4,
+    A5  = PIN_A5,
+    A6  = PIN_A6,
+
+    DAC0 = PIN_DAC0,
+    DAC1 = PIN_DAC1,
+
+    MOSI = PIN_SPI_MOSI,
+    MISO = PIN_SPI_MISO,
+    SCK  = PIN_SPI_SCK,
+
+    SDA = PIN_WIRE_SDA,
+    SCL = PIN_WIRE_SCL,
+};
+
+// These are macros, since portable sketches tend to #ifdef for these
+#define LED_BUILTIN   PIN_LED_RED
+#define LED_RED       PIN_LED_RED
+#define LED_BLUE      PIN_LED_BLUE
+#define LED_GREEN     PIN_LED_GREEN
+
+#define NUM_DIGITAL_PINS     (PINS_COUNT)
+#define NUM_ANALOG_INPUTS    (15u)
+#define NUM_ANALOG_OUTPUTS   (2u)
 
 #define ADC_RESOLUTION          12
-
-/*
- * Other pins
- */
-#define PIN_BUTTON           (PD2)
-static const uint8_t BUTTON = PIN_BUTTON;
-
-/*
- * Serial interfaces
- */
-
-#define SERIAL_INTERFACES_COUNT 3
-
-#define PIN_SERIAL1_RX        (PA10)
-#define PIN_SERIAL1_TX        (PA9)
-
-#define PIN_SERIAL2_RX        (PA3)
-#define PIN_SERIAL2_TX        (PA2)
-
-
-/*
- * SPI Interfaces
- */
-#define SPI_INTERFACES_COUNT 2
-
-#define PIN_SPI_MISO         (PB4)
-#define PIN_SPI_MOSI         (PB5)
-#define PIN_SPI_SCK          (PB3)
-
-#define PIN_SPI1_MISO        (PB14)
-#define PIN_SPI1_MOSI        (PB15)
-#define PIN_SPI1_SCK         (PB13)
-
-static const uint8_t SS   = 10;
-static const uint8_t MOSI = PIN_SPI_MOSI;
-static const uint8_t MISO = PIN_SPI_MISO;
-static const uint8_t SCK  = PIN_SPI_SCK;
-
-/*
- * Wire Interfaces
- */
-#define WIRE_INTERFACES_COUNT 3
-
-#define PIN_WIRE_SDA         (PB7)
-#define PIN_WIRE_SCL         (PB6)
-
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
-
-#define PIN_WIRE1_SDA        (PB11)
-#define PIN_WIRE1_SCL        (PB10)
-
-#define PIN_WIRE2_SDA        (PC1)
-#define PIN_WIRE2_SCL        (PC0)
-
-#define PIN_USB_DM           (PA11)
-#define PIN_USB_DP           (PA12)
-
-
-#define PWM_INSTANCE_COUNT    0
+#define DAC_RESOLUTION          12
+// This includes the virtual USB Serial
+#define SERIAL_INTERFACES_COUNT 4
+#define SPI_INTERFACES_COUNT    2
+#define WIRE_INTERFACES_COUNT   3
+#define PWM_INSTANCE_COUNT      3
 
 #ifdef __cplusplus
 }
@@ -224,6 +260,7 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 extern CDC  SerialUSB;
 extern Uart Serial1;
 extern Uart Serial2;
+extern Uart Serial3;
 #if defined(SERIAL_IS_CONFIGURABLE)
     // This allows reconfiguring Serial at runtime, to allow e.g.
     // switching between SerialUSB and Serial1 based on whether USB is
