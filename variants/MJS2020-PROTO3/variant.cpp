88461e707641d9a94d7c029a36a75c0c12b9e0d7
Matthijs Kooijman <matthijs@stdin.nl>
2021-02-24 18:06:41 +0100

    Add MJS2020-PROTO3 board definition

---
+++ variants/MJS2020-PROTO3/variant.cpp
@@ -0,0 +1,283 @@
+/*
+ * Copyright (c) 2017-2018 Thomas Roell.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of Thomas Roell, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ */
+
+#include "Arduino.h"
+#include "wiring_private.h"
+
+enum {
+    PWM_INSTANCE_TIM2,
+    PWM_INSTANCE_TIM3,
+    PWM_INSTANCE_TIM21,
+};
+
+/*
+ * Pins descriptions
+ */
+extern const PinDescription g_APinDescription[PINS_COUNT] =
+{
+    // The ordering of these is not as clean as you might want. The Dx
+    // pins need to be a the start (so digitalRead(0) gets you D0), and
+    // all Ax pins must be consecutive and not among the first
+    // NUM_ANALOG_INPUTS (so analogRead(0) gets you A0).
+    //
+    // So the basic idea is to start with all pins on the prototype
+    // header, in a bit of a mangled order because of the Dx pin
+    // placement and to make sure that the analog pins have high enough
+    // pin numbers. Then, the rest of the analog pins (in Ax order),
+    // followed by all other pins in no particular order.
+
+    // 0..2 - Dx pins
+    [PA15] = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA15), STM32L0_GPIO_PIN_PA0_TIM2_CH1,   (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM2,  PWM_CHANNEL_1,    ADC_CHANNEL_NONE },
+    [PB8]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB8),  STM32L0_GPIO_PIN_PB8,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PB9]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB9),  STM32L0_GPIO_PIN_PB9,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 3..4 SCL/SDA
+    [PB6]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB6),  STM32L0_GPIO_PIN_PB6,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PB7]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB7),  STM32L0_GPIO_PIN_PB7,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 5..6 SCL1/SDA1
+    [PB10] = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB10), STM32L0_GPIO_PIN_PB10_TIM2_CH3,  (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM2,  PWM_CHANNEL_3,    ADC_CHANNEL_NONE },
+    [PB11] = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB11), STM32L0_GPIO_PIN_PB11_TIM2_CH4,  (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM2,  PWM_CHANNEL_4,    ADC_CHANNEL_NONE },
+
+    // 7..10 SPI1
+    [PB15] = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB15), STM32L0_GPIO_PIN_PB15,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PB14] = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB14), STM32L0_GPIO_PIN_PB14_TIM21_CH2, (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM21, PWM_CHANNEL_2,    ADC_CHANNEL_NONE },
+    [PB13] = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB13), STM32L0_GPIO_PIN_PB13_TIM21_CH1, (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM21, PWM_CHANNEL_1,    ADC_CHANNEL_NONE },
+    [PB12] = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB12), STM32L0_GPIO_PIN_PB12,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 11 - TX4
+    [PC10] = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC10), STM32L0_GPIO_PIN_PC10,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 12..14 - A0..A2 (A0 must be > 9 to support up to A9)
+    [PA4]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA4),  STM32L0_GPIO_PIN_PA4,            (PIN_ATTR_EXTI|PIN_ATTR_DAC1),                 PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_4,   },
+    [PA5]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA5),  STM32L0_GPIO_PIN_PA5,            (PIN_ATTR_EXTI|PIN_ATTR_DAC2),                 PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_5    },
+    [PB0]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB0),  STM32L0_GPIO_PIN_PB0,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_8    },
+    // 15 - A3 / RX4
+    [PA1]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA1),  STM32L0_GPIO_PIN_PA1_TIM2_CH2,   (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM2,  PWM_CHANNEL_2,    ADC_CHANNEL_1    },
+    // 16 - A4 / Lux analog
+    [PC3]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC3),  STM32L0_GPIO_PIN_PC3,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_13   },
+    // 17..18 - A5..A6 / Grove Multi
+    [PC0]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC0),  STM32L0_GPIO_PIN_PC0,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_10   },
+    [PC1]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC1),  STM32L0_GPIO_PIN_PC1,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_11   },
+
+    // 19 - A7 / Battery divider
+    [PC4]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC4),  STM32L0_GPIO_PIN_PC4,            0,                                             PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_14   },
+
+    // 20 - A8 / Solar divider
+    [PC5]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC5),  STM32L0_GPIO_PIN_PC5,            0,                                             PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_15   },
+
+    // 21 - A9 / USB VBUS divider
+    [PA7]  = { GPIOA,  STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA7), STM32L0_GPIO_PIN_PA7,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_7    },
+
+    // 22..24 - RGB LED
+    [PC7]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC7),  STM32L0_GPIO_PIN_PC7_TIM3_CH2,   (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM3,  PWM_CHANNEL_2, ADC_CHANNEL_NONE    },
+    [PC8]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC8),  STM32L0_GPIO_PIN_PC8_TIM3_CH3,   (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM3,  PWM_CHANNEL_3, ADC_CHANNEL_NONE    },
+    [PC6]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC6),  STM32L0_GPIO_PIN_PC6_TIM3_CH1,   (PIN_ATTR_EXTI),                               PWM_INSTANCE_TIM3,  PWM_CHANNEL_1, ADC_CHANNEL_NONE    },
+
+    // 25..27 PSU Enable (3V_SENS, 3V_GPS, 5V)
+    [PC11] = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC11), STM32L0_GPIO_PIN_PC11,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PA8]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA8),  STM32L0_GPIO_PIN_PA8,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PC12] = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC12), STM32L0_GPIO_PIN_PC12,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 28 - Charger status
+    [PC9]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC9),  STM32L0_GPIO_PIN_PC9,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 29 - Lux range selection
+    [PA6]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA6),  STM32L0_GPIO_PIN_PA6,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_6    },
+
+    // 30 - User Button
+    [PD2]  = { GPIOD, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PD2),  STM32L0_GPIO_PIN_PD2,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 31..34 LoRA I/O (NSS, RST, DIO1, BUSY)
+    [PC2]  = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC2),  STM32L0_GPIO_PIN_PC2,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_12   },
+    [PC13] = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC13), STM32L0_GPIO_PIN_PC13,           (PIN_ATTR_EXTI|PIN_ATTR_WKUP2),                PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PB1]  = { GPIOB,  STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB1), STM32L0_GPIO_PIN_PB1,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_9    },
+    [PB2]  = { GPIOB,  STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB2), STM32L0_GPIO_PIN_PB2,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 35..37 LoRa SPI (MOSI, MISO, SCK)
+    [PB5]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB5),  STM32L0_GPIO_PIN_PB5,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PB4]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB4),  STM32L0_GPIO_PIN_PB4,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PB3]  = { GPIOB, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PB3),  STM32L0_GPIO_PIN_PB3,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 38..39 FTDI (UART TX, UART RX)
+    [PA9]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA9),  STM32L0_GPIO_PIN_PA9,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_9    },
+    [PA10] = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA10), STM32L0_GPIO_PIN_PA10,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 40..43 - GPS (UART2 TX, UART2 RX, 2xTIMEPULSE)
+    [PA2]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA2),  STM32L0_GPIO_PIN_PA2,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_2    },
+    [PA3]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA3),  STM32L0_GPIO_PIN_PA3,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_3    },
+    [PA0]  = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA0),  STM32L0_GPIO_PIN_PA0,            (PIN_ATTR_EXTI|PIN_ATTR_WKUP1),                PWM_INSTANCE_TIM2,  PWM_CHANNEL_1,    ADC_CHANNEL_0    },
+    [PH0]  = { GPIOH, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PH0),  STM32L0_GPIO_PIN_PH0,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 44..45 - SWD (SWDIO, SWDCLK)
+    [PA13] = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA13), STM32L0_GPIO_PIN_PA13,           (PIN_ATTR_EXTI|PIN_ATTR_SWD),                  PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    [PA14] = { GPIOA, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA14), STM32L0_GPIO_PIN_PA14,           (PIN_ATTR_EXTI|PIN_ATTR_SWD),                  PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // 46 - SPI Flash CS (optional chip)
+    [PH1]  = { GPIOH, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PH1),  STM32L0_GPIO_PIN_PH1,            (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // USB (DM, DP)
+    // [PA11] = { GPIOA,  STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA11), STM32L0_GPIO_PIN_PA11,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    // [PA12] = { GPIOA,  STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PA12), STM32L0_GPIO_PIN_PA12,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+
+    // RTC Crystal
+    // [PC14] = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC14), STM32L0_GPIO_PIN_PC14,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+    // [PC15] = { GPIOC, STM32L0_GPIO_PIN_MASK(STM32L0_GPIO_PIN_PC15), STM32L0_GPIO_PIN_PC15,           (PIN_ATTR_EXTI),                               PWM_INSTANCE_NONE,  PWM_CHANNEL_NONE, ADC_CHANNEL_NONE },
+};
+
+extern const unsigned int g_PWMInstances[PWM_INSTANCE_COUNT] = {
+    [PWM_INSTANCE_TIM2] = STM32L0_TIMER_INSTANCE_TIM2,
+    [PWM_INSTANCE_TIM3] = STM32L0_TIMER_INSTANCE_TIM3,
+    [PWM_INSTANCE_TIM21] = STM32L0_TIMER_INSTANCE_TIM21,
+};
+
+static uint8_t stm32l0_usart1_rx_fifo[32];
+
+extern const stm32l0_uart_params_t g_Serial1Params = {
+    STM32L0_UART_INSTANCE_USART1,
+    STM32L0_UART_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_DMA1_CH3_USART1_RX,
+    STM32L0_DMA_CHANNEL_NONE,
+    &stm32l0_usart1_rx_fifo[0],
+    sizeof(stm32l0_usart1_rx_fifo),
+    {
+        STM32L0_GPIO_PIN_PA10_USART1_RX,
+        STM32L0_GPIO_PIN_PA9_USART1_TX,
+        STM32L0_GPIO_PIN_NONE,
+        STM32L0_GPIO_PIN_NONE,
+    },
+};
+
+
+static uint8_t stm32l0_usart2_rx_fifo[32];
+
+extern const stm32l0_uart_params_t g_Serial2Params = {
+    STM32L0_UART_INSTANCE_USART2,
+    STM32L0_UART_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_DMA1_CH6_USART2_RX,
+    STM32L0_DMA_CHANNEL_DMA1_CH4_USART2_TX,
+    &stm32l0_usart2_rx_fifo[0],
+    sizeof(stm32l0_usart2_rx_fifo),
+    {
+        STM32L0_GPIO_PIN_PA3_USART2_RX,
+        STM32L0_GPIO_PIN_PA2_USART2_TX,
+        STM32L0_GPIO_PIN_NONE,
+        STM32L0_GPIO_PIN_NONE,
+    },
+};
+
+
+static uint8_t stm32l0_usart4_rx_fifo[32];
+
+extern const stm32l0_uart_params_t g_Serial3Params = {
+    STM32L0_UART_INSTANCE_USART4,
+    STM32L0_UART_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_NONE,
+    STM32L0_DMA_CHANNEL_NONE,
+    &stm32l0_usart4_rx_fifo[0],
+    sizeof(stm32l0_usart4_rx_fifo),
+    {
+        STM32L0_GPIO_PIN_PA1_USART4_RX,
+        STM32L0_GPIO_PIN_PA0_USART4_TX,
+        STM32L0_GPIO_PIN_NONE,
+        STM32L0_GPIO_PIN_NONE,
+    },
+};
+
+
+extern const stm32l0_spi_params_t g_SPI1Params = {
+    STM32L0_SPI_INSTANCE_SPI2,
+    STM32L0_SPI_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_NONE,
+    STM32L0_DMA_CHANNEL_NONE,
+    {
+        STM32L0_GPIO_PIN_PB15_SPI2_MOSI,
+        STM32L0_GPIO_PIN_PB14_SPI2_MISO,
+        STM32L0_GPIO_PIN_PB13_SPI2_SCK,
+        STM32L0_GPIO_PIN_NONE,
+    },
+};
+
+
+extern const stm32l0_spi_params_t g_SPIParams = {
+    STM32L0_SPI_INSTANCE_SPI1,
+    STM32L0_SPI_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_NONE,
+    STM32L0_DMA_CHANNEL_NONE,
+    {
+        STM32L0_GPIO_PIN_PB5_SPI1_MOSI,
+        STM32L0_GPIO_PIN_PB4_SPI1_MISO,
+        STM32L0_GPIO_PIN_PB3_SPI1_SCK,
+        STM32L0_GPIO_PIN_NONE,
+    },
+};
+
+
+extern const stm32l0_i2c_params_t g_WireParams = {
+    STM32L0_I2C_INSTANCE_I2C1,
+    STM32L0_I2C_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_DMA1_CH7_I2C1_RX,
+    STM32L0_DMA_CHANNEL_NONE,
+    {
+        STM32L0_GPIO_PIN_PB6_I2C1_SCL,
+        STM32L0_GPIO_PIN_PB7_I2C1_SDA,
+    },
+};
+
+
+extern const stm32l0_i2c_params_t g_Wire1Params = {
+    STM32L0_I2C_INSTANCE_I2C2,
+    STM32L0_I2C_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_DMA1_CH5_I2C2_RX,
+    STM32L0_DMA_CHANNEL_NONE,
+    {
+        STM32L0_GPIO_PIN_PB10_I2C2_SCL,
+        STM32L0_GPIO_PIN_PB11_I2C2_SDA,
+    },
+};
+
+extern const stm32l0_i2c_params_t g_Wire2Params = {
+    STM32L0_I2C_INSTANCE_I2C3,
+    STM32L0_I2C_IRQ_PRIORITY,
+    STM32L0_DMA_CHANNEL_NONE,
+    STM32L0_DMA_CHANNEL_NONE,
+    {
+        STM32L0_GPIO_PIN_PC0_I2C3_SCL,
+        STM32L0_GPIO_PIN_PC1_I2C3_SDA,
+    },
+};
+
+#if defined(SERIAL_IS_CONFIGURABLE)
+// When the sketch does not set this, let Serial point to USB
+Stream* ConfigurableSerial = &SerialUSB;
+#endif
+
+void initVariant()
+{
+    //CMWX1ZZABZ_Initialize(STM32L0_GPIO_PIN_PH1, STM32L0_GPIO_PIN_NONE);
+}
---
+++ variants/MJS2020-PROTO3/variant.h
@@ -0,0 +1,311 @@
+/*
+ * Copyright (c) 2017-2018 Thomas Roell.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimers.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimers in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of Thomas Roell, nor the names of its contributors
+ *     may be used to endorse or promote products derived from this Software
+ *     without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ */
+
+#pragma once
+
+/*----------------------------------------------------------------------------
+ *        Definitions
+ *----------------------------------------------------------------------------*/
+
+#define STM32L0_CONFIG_LSECLK             32768
+#define STM32L0_CONFIG_HSECLK             0
+#define STM32L0_CONFIG_SYSOPT             0
+
+#define STM32L0_CONFIG_PIN_VBUS           STM32L0_GPIO_PIN_PA7
+
+#define STM32L0_CONFIG_PIN_VBAT           STM32L0_GPIO_PIN_PC4
+#define STM32L0_CONFIG_CHANNEL_VBAT       ADC_CHANNEL_14
+#define STM32L0_CONFIG_VBAT_PERIOD        40
+#define STM32L0_CONFIG_VBAT_SCALE         ((float)1.27)
+
+
+
+#define USBCON
+
+/** Master clock frequency */
+#define VARIANT_MCK                       F_CPU
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+#include "USBAPI.h"
+#include "Uart.h"
+#endif // __cplusplus
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+/*----------------------------------------------------------------------------
+ *        Pins
+ *----------------------------------------------------------------------------*/
+
+// Map chip pin names to the corresponding Arduino logical pin number
+enum {
+    PA15 = 0,
+    PB8  = 1,
+    PB9  = 2,
+    PB6  = 3,
+    PB7  = 4,
+    PB10 = 5,
+    PB11 = 6,
+    PB15 = 7,
+    PB14 = 8,
+    PB13 = 9,
+    PB12 = 10,
+    PC10 = 11,
+    PA4  = 12,
+    PA5  = 13,
+    PB0  = 14,
+    PA1  = 15,
+    PC3  = 16,
+    PC0  = 17,
+    PC1  = 18,
+    PC4  = 19,
+    PC5  = 20,
+    PA7  = 21,
+    PC7  = 22,
+    PC8  = 23,
+    PC6  = 24,
+    PC11 = 25,
+    PA8  = 26,
+    PC12 = 27,
+    PC9  = 28,
+    PA6  = 29,
+    PD2  = 30,
+    PC2  = 31,
+    PC13 = 32,
+    PB1  = 33,
+    PB2  = 34,
+    PB5  = 35,
+    PB4  = 36,
+    PB3  = 37,
+    PA9  = 38,
+    PA10 = 39,
+    PA2  = 40,
+    PA3  = 41,
+    PA0  = 42,
+    PH0  = 43,
+    PA13 = 44,
+    PA14 = 45,
+    PH1  = 46,
+    PINS_COUNT,
+};
+
+// Define friendly names for all pins
+enum {
+    // Digital pins
+    PIN_D0                = PA15,
+    PIN_D1                = PB8,
+    PIN_D2                = PB9,
+
+    // Analog pins
+    // There are more analog inputs, these are just the ones where
+    // analog input actually makes sense.
+    PIN_A0                = PA4,
+    PIN_DAC0              = PA4,
+    PIN_A1                = PA5,
+    PIN_DAC1              = PA5,
+    PIN_A2                = PB0,
+    PIN_A3                = PA1,
+    PIN_A4                = PC3,
+    PIN_A5                = PC0,
+    PIN_A6                = PC1,
+    PIN_A7                = PC4,
+    PIN_A8                = PC5,
+    PIN_A9                = PA0,
+
+    // LEDs
+    PIN_LED_RED           = PC7,
+    PIN_LED_GREEN         = PC8,
+    PIN_LED_BLUE          = PC6,
+
+    // Regulator enable pins
+    PIN_ENABLE_3V_SENS    = PC11,
+    PIN_ENABLE_3V_GPS     = PA8,
+    PIN_ENABLE_5V         = PC12,
+
+    // Voltage & charging pins
+    PIN_BATTERY           = PC4,
+    PIN_SOLAR             = PC5,
+    PIN_VBUS              = PA0,
+    PIN_CHARGE_STATUS     = PC9,
+
+    // Lux measurement
+    PIN_LUX_RANGE         = PA6,
+    PIN_LUX_ADC           = PC3,
+
+    // Button
+    PIN_BUTTON            = PD2,
+
+    // LoRa Radio pins
+    PIN_LORA_SS           = PC2,
+    PIN_LORA_RST          = PC13,
+    PIN_LORA_DIO1         = PB1,
+    PIN_LORA_BUSY         = PB2,
+
+    // Other
+    PIN_GPS_TIMEPULSE     = PA0,
+
+    // SPI, I²C and serial
+    PIN_MISO              = PB4,
+    PIN_MOSI              = PB5,
+    PIN_SCK               = PB3,
+
+    PIN_MISO1             = PB14,
+    PIN_MOSI1             = PB15,
+    PIN_SCK1              = PB13,
+    PIN_NSS1              = PB12,
+
+    PIN_SCL               = PB6,
+    PIN_SDA               = PB7,
+
+    PIN_SCL1              = PB10,
+    PIN_SDA1              = PB11,
+
+    PIN_SCL2              = PC0,
+    PIN_SDA2              = PC1,
+
+    PIN_SERIAL1_RX        = PA10,
+    PIN_SERIAL1_TX        = PA9,
+
+    PIN_SERIAL2_RX        = PA3,
+    PIN_SERIAL2_TX        = PA2,
+
+    PIN_SERIAL3_RX        = PA1,
+    PIN_SERIAL3_TX        = PC10,
+
+    // SWD
+    PIN_SWDIO             = PA13,
+    PIN_SWDCLK            = PA14,
+};
+
+// Define even shorter names for some pins (these are names commonly
+// defined by other Arduino cores as well).
+enum {
+    A0  = PIN_A0,
+    A1  = PIN_A1,
+    A2  = PIN_A2,
+    A3  = PIN_A3,
+    A4  = PIN_A4,
+    A5  = PIN_A5,
+    A6  = PIN_A6,
+
+    DAC0 = PIN_DAC0,
+    DAC1 = PIN_DAC1,
+
+    MOSI = PIN_MOSI,
+    MISO = PIN_MISO,
+    SCK  = PIN_SCK,
+
+    SDA = PIN_SDA,
+    SCL = PIN_SCL,
+};
+
+// These are macros, since portable sketches tend to #ifdef for these
+#define LED_BUILTIN   PIN_LED_RED
+#define LED_RED       PIN_LED_RED
+#define LED_BLUE      PIN_LED_BLUE
+#define LED_GREEN     PIN_LED_GREEN
+
+#define NUM_DIGITAL_PINS     (PINS_COUNT)
+#define NUM_ANALOG_INPUTS    (9u)
+#define NUM_ANALOG_OUTPUTS   (2u)
+
+#define ADC_RESOLUTION          12
+#define DAC_RESOLUTION          12
+// This includes the virtual USB Serial
+#define SERIAL_INTERFACES_COUNT 4
+#define SPI_INTERFACES_COUNT    2
+#define WIRE_INTERFACES_COUNT   3
+#define PWM_INSTANCE_COUNT      3
+
+#ifdef __cplusplus
+}
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+extern CDC  SerialUSB;
+extern Uart Serial1;
+extern Uart Serial2;
+extern Uart Serial3;
+#if defined(SERIAL_IS_CONFIGURABLE)
+    // This allows reconfiguring Serial at runtime, to allow e.g.
+    // switching between SerialUSB and Serial1 based on whether USB is
+    // connected or not. To ensure that sketches and libraries do not
+    // need to know the difference, this defines a pointer under a
+    // different name, which is then dereferenced by the Serial macro
+    // below (so normal member access with . still works).
+    //
+    // It would seem nice to make Serial reconfigurable by default, but
+    // since the common superclass for SerialUSB and Serial1 is Stream,
+    // which does not have a .begin() method, this would break sketches
+    // that call Serial.begin(). So instead, reconfigurable serial is a
+    // compile-time option, and sketches that need it must call
+    // Serial1.begin() and/or SerialUSB.begin() as appropriate, and can
+    // then assign ConfigurableSerial as needed.
+    extern Stream* ConfigurableSerial;
+#endif
+#endif
+
+// These serial port names are intended to allow libraries and architecture-neutral
+// sketches to automatically default to the correct port name for a particular type
+// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+// the first hardware serial port whose RX/TX pins are not dedicated to another use.
+//
+// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+//
+// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+//
+// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+//
+// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+//
+// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+//                            pins are NOT connected to anything by default.
+#define SERIAL_PORT_USBVIRTUAL      SerialUSB
+#define SERIAL_PORT_MONITOR         Serial
+#define SERIAL_PORT_HARDWARE1       Serial1
+#define SERIAL_PORT_HARDWARE2       Serial2
+#define SERIAL_PORT_HARDWARE_OPEN2  Serial1
+
+// Alias Serial to SerialUSB by default, but allow reconfiguring it
+#if defined(SERIAL_IS_CONFIGURABLE)
+    #define Serial                      (*ConfigurableSerial)
+#elif defined(SERIAL_IS_SERIAL1)
+    #define Serial                      Serial1
+#else
+    #define Serial                      SerialUSB
+#endif
